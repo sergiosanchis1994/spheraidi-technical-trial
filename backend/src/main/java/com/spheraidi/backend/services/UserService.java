54782bcb080b7ecf3d2f5a4ec54a9433ba37c1d1
Sergio <sergiosanchis1994@gmail.com>
2019-06-30 22:51:47 +0200

    Add user service

---
+++ backend/src/main/java/com/spheraidi/backend/services/UserService.java
@@ -0,0 +1,12 @@
+package com.spheraidi.backend.services;
+
+import com.spheraidi.backend.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+}
