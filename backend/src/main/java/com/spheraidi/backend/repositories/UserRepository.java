24b8bf71008e3cb927d474e9a041ccebbd73dcae
Sergio <sergiosanchis1994@gmail.com>
2019-06-30 23:28:32 +0200

    Add createUser and login in backend without check preconditions

--- backend/src/main/java/com/spheraidi/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/spheraidi/backend/repositories/UserRepository.java
@@ -2,8 +2,13 @@ package com.spheraidi.backend.repositories;
 
 import com.spheraidi.backend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository  extends JpaRepository<User, String> {
+
+    User findByUsername(@Param("username") String username);
+
+    User findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
 }
--- backend/src/main/java/com/spheraidi/backend/services/UserService.java
+++ backend/src/main/java/com/spheraidi/backend/services/UserService.java
@@ -1,12 +1,41 @@
 package com.spheraidi.backend.services;
 
+import com.spheraidi.backend.models.User;
 import com.spheraidi.backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public User createUser(String username, String password, String sessionId){
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setSessionId(sessionId);
+        user.setActive(true);
+        return userRepository.save(user);
+    }
+
+    public User login(String username, String password, String sessionId){
+        User user = userRepository.findByUsernameAndPassword(username, password);
+        if(Objects.nonNull(user)){
+            user.setSessionId(sessionId);
+            user.setActive(true);
+            userRepository.save(user);
+        }
+        return user;
+    }
+
+    public void logout(String username){
+        User user = userRepository.findByUsername(username);
+        user.setActive(false);
+        user.setSessionId(null);
+        userRepository.save(user);
+    }
 }
